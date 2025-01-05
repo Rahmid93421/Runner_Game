ff349da90468c4c8bcb848994dede16767996a51
Andrei <rahmid.93421@gmail.com>
2025-01-05 12:49:25 +0200

    Main Menu implemented + background environment

--- scripts/Master.gd
+++ scripts/Master.gd
@@ -1,12 +1,14 @@
 extends Node
 
+onready var mainMenuNode = preload("res://scenes/main_menu/MainMenu.tscn")
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
 func _intro_finished():
-	pass
+	_load_main_menu()
+
+func _load_main_menu():
+	var instance = mainMenuNode.instance()
+	self.add_child(instance)
